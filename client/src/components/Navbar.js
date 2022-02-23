661bd753e7dfd0b90d193f37f2afc5ed66f2be72
Eric Fung <ehhfung@gmail.com>
2022-02-23 15:03:05 -0500

    removed repetitive code with loops in navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -19,6 +19,13 @@ const Navbar = () => {
     ];
   }, []);
 
+  const userMenuItems = useMemo(() => {
+    return [
+      ['men', 'Your Profile'],
+      ['women', 'Sign out'],
+    ];
+  }, []);
+
   const toggleUserMenu = () => {
     setUserMenu(!userMenu);
   };
@@ -39,23 +46,18 @@ const Navbar = () => {
               onClick={toggleShopMenu}
             >
               <span className="sr-only">Open main menu</span>
-              {!shopMenu && (
-                <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-                </svg>
-              )}
-              {shopMenu && (
-                <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-                </svg>
-              )}
+              <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={`${!shopMenu ? 'M4 6h16M4 12h16M4 18h16' : 'M6 18L18 6M6 6l12 12'}`} />
+              </svg>
             </button>
           </div>
           <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex-shrink-0 flex items-center">
-              <img className="block lg:hidden h-16 w-auto" src={logo} alt="" />
-              <img className="hidden lg:block h-16 w-auto" src={logo} alt="" />
-            </div>
+            <Link to={'/'}>
+              <div className="flex-shrink-0 flex items-center">
+                <img className="block lg:hidden h-16 w-auto" src={logo} alt="" />
+                <img className="hidden lg:block h-16 w-auto" src={logo} alt="" />
+              </div>
+            </Link>
             <div className="hidden items-center sm:flex sm:ml-6">
               <div className="flex space-x-4">
                 {menuItems.map(([link, title]) => (
@@ -102,22 +104,23 @@ const Navbar = () => {
                   aria-labelledby="user-menu-button"
                   tabindex="-1"
                 >
-                  <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">
-                    Your Profileasdasd
-                  </a>
-                  <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">
-                    Settings
-                  </a>
-                  <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">
-                    Sign out
-                  </a>
+                  {userMenuItems.map(([link, title], index) => (
+                    <div
+                      onClick={toggleUserMenu}
+                      className="block px-4 py-2 text-sm text-gray-700 cursor-pointer hover:text-orange-500 hover:underline"
+                      role="menuitem"
+                      tabindex="-1"
+                      id={`user-menu-item-${index}`}
+                    >
+                      {title}
+                    </div>
+                  ))}
                 </div>
               )}
             </div>
           </div>
         </div>
       </div>
-
       {shopMenu && (
         <div className="sm:hidden" id="mobile-menu">
           <div className="px-2 pt-2 pb-3 space-y-1">
