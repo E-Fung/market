c33e6d3e026ee08bee7c5df91b423b1366c7f897
Eric Fung <ehhfung@gmail.com>
2022-02-25 19:42:14 -0500

    ui changes via tailwind css theme

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,19 +1,10 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-<<<<<<< HEAD
 import loggerMiddleware from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 
 import rootReducer from './reducers/';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-=======
-import rootReducer from './reducers/';
-import loggerMiddleware from 'redux-logger';
-import thunkMiddleware from 'redux-thunk';
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
->>>>>>> 5f55c9b861e717032550051fef3882a7687b142f
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware, loggerMiddleware)));
 
 export default store;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: 'rgb(4, 28, 50)',
+        light: 'rgb(6, 70, 99)',
+        highlight: 'rgb(236, 179, 101)',
+        accent: 'rgb(4, 41, 58)',
+      },
+    },
   },
   plugins: [],
 };
+
+//https://colorhunt.co/palette/041c3204293a064663ecb365
+//rgb(4, 28, 50)
+// rgb(4, 41, 58)
+// rgb(6, 70, 99)
+// rgb(236, 179, 101)
